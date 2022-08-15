f2d8154c07245f8e0e7f6685f896eb35dedc3c4e
Roderick Fayson <rgfayson2@gmail.com>
2022-08-15 17:06:34 -0400

    final push

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,51 +1,54 @@
-import React, { Component } from "react"
+import React,{Component} from 'react'
 
 class ClassComponent extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      count: props.count,
-      coffee: [],
+    // props
+    // setting state
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: props.count,
+            coffee: []
+        }
     }
-  }
 
-  componentDidMount() {
-    // like useEffect
-    console.log("component did mount")
-    let url = "https://api.sampleapis.com/coffee/hot"
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          count: this.state.count,
-          coffee: data,
-        })
-      })
-  }
-
-  render() {
-    return (
-      <>
-        <h1>this is a class component {this.state.count}</h1>
-        <button
-          onClick={() => {
+    // useEffect
+    componentDidMount() { // loading coffees from api
+        console.log("component did mount")
+        let url = "https://api.sampleapis.com/coffee/hot"
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => 
             this.setState({
-              count: this.state.count + 1,
-              coffee: this.state.coffee,
-            })
-          }}
-        >
-          add one
-        </button>
-        <p>
-          {this.state.coffee.map((coffee) => (
-            <h1>{coffee.title}</h1>
-          ))}
-        </p>
-      </>
-    )
-  }
+             count: this.state.count,
+             coffee: data
+             })
+        )
+    }
+
+    // return 
+    render() {
+        return (
+            <>
+             <h1> this is a class component {this.state.count}</h1>
+             <button onClick={()=> {
+                    // update the count to count+1
+                    this.setState(
+                        {
+                            count: this.state.count + 1,
+                            coffee: this.state.coffee
+                        })
+             }}>add one</button>
+                {/* map through coffee */}
+                {this.state.coffee.map(
+                    coffee => 
+                    <h1 key={coffee.id}>{coffee.title}</h1>
+                )}
+            </>
+        )
+    }
+
 }
 
 export default ClassComponent
--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React,{useState,useEffect} from 'react';
 
-export default function FunctionalComponent({count}) {
+export default function 
+FunctionalComponent({passCount}) {
+
+    const [count,setCount] = useState(passCount)
+    const [coffee,setCoffee] = useState([])
+
+    useEffect(()=> {
+
+        let url = "https://api.sampleapis.com/coffee/hot"
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => {
+            // do something with data
+            setCoffee(data)
+        })
+
+    },[])
+
+
     return (
         <>
-        OMG this is so functional {count}
+         <h1> this is a functional component {count}</h1>
+         <button onClick={()=> {
+                setCount(count+1)
+         }}>add one</button>
+            {/* map through coffee */}
+            {coffee.map(
+                coffee => 
+                <h1 key={coffee.id}>{coffee.title}</h1>
+            )}
         </>
     )
 }
-       
-       
+
+//export default FunctionalComponents
